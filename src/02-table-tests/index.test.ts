4ed0c3d925172390556806f0eca1e9f47a0e0257
yelantsevv <yelantsevv@gmail.com>
2024-10-10 22:04:20 +0300

    add: test 2

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -2,7 +2,7 @@
 import { simpleCalculator, Action } from './index';
 
 const testCases = [
-  { a: 1, b: 3, action: Action.Add, expected: 3 },
+  { a: 1, b: 2, action: Action.Add, expected: 3 },
   { a: 2, b: 2, action: Action.Add, expected: 4 },
   { a: 3, b: 2, action: Action.Add, expected: 5 },
   { a: 1, b: 2, action: Action.Subtract, expected: -1 },
