c559dcdf3051a3e3e49731676028a48275e96916
yelantsevv <yelantsevv@gmail.com>
2024-10-13 21:47:51 +0300

    add: partial-mocking

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,20 +1,29 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+  return {
+    ...originalModule,
+    mockOne: jest.fn(),
+    mockTwo: jest.fn(),
+    mockThree: jest.fn(),
+  };
 });
 
 describe('partial mocking', () => {
-  afterAll(() => {
-    jest.unmock('./index');
-  });
-
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    const mockLog = jest.spyOn(global.console, 'log');
+    mockOne();
+    mockTwo();
+    mockThree();
+    expect(mockLog).toHaveBeenCalledTimes(0);
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    const mockLog = jest.spyOn(global.console, 'log');
+    unmockedFunction();
+    expect(mockLog).toHaveBeenCalledTimes(1);
   });
 });
