e5c2c547dcbeb08e109fbe1a692f8ed7fbc8bc5d
yelantsevv <yelantsevv@gmail.com>
2024-10-16 15:50:01 +0300

    add: mocking-axios-ipa

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,35 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => jest.useFakeTimers());
+  afterAll(() => jest.useRealTimers());
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const spy = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi('/posts');
+    expect(spy).toBeCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockRequest = jest
+      .spyOn(axios.Axios.prototype, 'get')
+      .mockResolvedValueOnce({ data: null });
+
+    await throttledGetDataFromApi('/posts');
+    await jest.runOnlyPendingTimersAsync();
+
+    expect(mockRequest).toHaveBeenCalledWith('/posts');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockData = { data: 'test' };
+    jest.spyOn(axios.Axios.prototype, 'get').mockResolvedValueOnce(mockData);
+
+    const res = await throttledGetDataFromApi('/posts');
+    expect(res).toBe(mockData.data);
   });
 });
